f21475b060059314f686113d8ec4eb2e92c26af6
Adi Campan <adicampan1974@gmail.com>
2024-11-10 11:48:36 +0100

    add "change Password" function

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,125 @@
+// import React, { useState } from "react";
+// import {
+//   signInWithEmailAndPassword,
+//   sendPasswordResetEmail,
+// } from "firebase/auth";
+// import { auth } from "../../firebase-config";
+// import { NavLink, useNavigate } from "react-router-dom";
+// import { useForm } from "react-hook-form";
+// import "./Login.css";
+
+// const Login = () => {
+//   const navigate = useNavigate();
+//   const [loginError, setLoginError] = useState("");
+//   const [loading, setLoading] = useState(false);
+
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm();
+
+//   const onLogin = ({ email, password }) => {
+//     setLoading(true);
+//     signInWithEmailAndPassword(auth, email, password)
+//       .then((userCredential) => {
+//         setLoginError("");
+//         const user = userCredential.user;
+//         navigate("/persoane");
+//         console.log(user);
+//       })
+//       .catch((error) => {
+//         setLoginError("Error al iniciar sesión: " + error.message);
+//         console.log(error.code, error.message);
+//       })
+//       .finally(() => setLoading(false));
+//   };
+//   const handlePasswordReset = (email) => {
+//     if (!email) {
+//       setLoginError("Por favor, introduce tu correo electrónico.");
+//       return;
+//     }
+
+//     setLoading(true);
+//     sendPasswordResetEmail(auth, email)
+//       .then(() => {
+//         setLoginError(
+//           "Se ha enviado un enlace para restablecer la contraseña a tu correo electrónico."
+//         );
+//       })
+//       .catch((error) => {
+//         setLoginError(
+//           "Error al enviar el correo de recuperación: " + error.message
+//         );
+//       })
+//       .finally(() => setLoading(false));
+//   };
+
+//   return (
+//     <main className="login">
+//       <section>
+//         <div className="main-container">
+//           <p>LOG IN</p>
+
+//           <form onSubmit={handleSubmit(onLogin)}>
+//             <div>
+//               <input
+//                 {...register("email", {
+//                   required: "Email este obligatorie!",
+//                   pattern: {
+//                     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+//                     message: "Adresa de email nu este valida!",
+//                   },
+//                 })}
+//                 className="input-container"
+//                 type="email"
+//                 placeholder="Introdu adresa de email"
+//               />
+//               {errors.email && <p className="error">{errors.email.message}</p>}
+//             </div>
+
+//             <div className="input-container">
+//               <input
+//                 {...register("password", {
+//                   required: "Parola este obligatorie!",
+//                   minLength: {
+//                     value: 6,
+//                     message: "Parola trebuie să aibă cel puțin 6 caractere.",
+//                   },
+//                 })}
+//                 className="input-container"
+//                 type="password"
+//                 placeholder="Password"
+//               />
+//               {errors.password && (
+//                 <p className="error">{errors.password.message}</p>
+//               )}
+//             </div>
+
+//             {loginError && <p className="error">{loginError}</p>}
+
+//             <div className="button-container">
+//               <button className="button" disabled={loading}>
+//                 {loading ? "Iniciando sesión..." : "LOGIN"}
+//               </button>
+//             </div>
+//           </form>
+//           <p className="forgot-password" onClick={handlePasswordReset}>
+//             ¿Olvidaste tu contraseña?
+//           </p>
+//         </div>
+//       </section>
+//     </main>
+//   );
+// };
+
+// export default Login;
+
 import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 import { auth } from "../../firebase-config";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -14,6 +134,7 @@ const Login = () => {
     register,
     handleSubmit,
     formState: { errors },
+    getValues,
   } = useForm();
 
   const onLogin = ({ email, password }) => {
@@ -26,8 +147,30 @@ const Login = () => {
         console.log(user);
       })
       .catch((error) => {
-        setLoginError("Error al iniciar sesión: " + error.message);
+        setLoginError("Utilizator sau parola incorecta");
         console.log(error.code, error.message);
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const handlePasswordReset = () => {
+    const email = getValues("email");
+    if (!email) {
+      setLoginError("Por favor, introduce tu correo electrónico.");
+      return;
+    }
+
+    setLoading(true);
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        setLoginError(
+          "Se ha enviado un enlace para restablecer la contraseña a tu correo electrónico."
+        );
+      })
+      .catch((error) => {
+        setLoginError(
+          "Error al enviar el correo de recuperación: " + error.message
+        );
       })
       .finally(() => setLoading(false));
   };
@@ -42,15 +185,15 @@ const Login = () => {
             <div>
               <input
                 {...register("email", {
-                  required: "Email este obligatorie!",
+                  required: "Email este obligatoriu!",
                   pattern: {
                     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: "Adresa de email nu este valida!",
+                    message: "email invalid!",
                   },
                 })}
                 className="input-container"
                 type="email"
-                placeholder="Introdu adresa de email"
+                placeholder="Introdu email"
               />
               {errors.email && <p className="error">{errors.email.message}</p>}
             </div>
@@ -58,22 +201,20 @@ const Login = () => {
             <div className="input-container">
               <input
                 {...register("password", {
-                  required: "Parola este obligatorie!",
+                  required: "Parola e obligatorie!",
                   minLength: {
                     value: 6,
-                    message: "Parola trebuie să aibă cel puțin 6 caractere.",
+                    message: "Parola trebuia sa aiba cel putin 6 caractere !",
                   },
                 })}
                 className="input-container"
                 type="password"
-                placeholder="Password"
+                placeholder="Parola"
               />
               {errors.password && (
                 <p className="error">{errors.password.message}</p>
               )}
             </div>
-
-            {loginError && <p className="error">{loginError}</p>}
 
             <div className="button-container">
               <button className="button" disabled={loading}>
@@ -81,6 +222,12 @@ const Login = () => {
               </button>
             </div>
           </form>
+          {loginError && (
+            <p className="forgot-password" onClick={handlePasswordReset}>
+              Ai uitat parola?
+            </p>
+          )}
+          {loginError && <p className="error">{loginError}</p>}
         </div>
       </section>
     </main>
